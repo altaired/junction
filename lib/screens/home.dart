8de644515e591021066a6c841e3d13f3bc710758
Simon Persson <simon@akep.se>
2019-11-16 00:45:25 +0200

    Draft of week card

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+
+  final List<String> items = ["Hej"];
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Placeholder(),
-          Container(
-            height: 40,
-            color: Colors.green,
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              "Pay to win!",
-              style: TextStyle(
-                fontSize: 24,
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16, left: 16, right: 16),
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.all(16),
+                color: Colors.green,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Week 1",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Container(
+                        height: 200,
+                        child: Placeholder(),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           )
