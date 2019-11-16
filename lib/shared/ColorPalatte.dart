01cc5f5c1337a1067feeb2bb43214cb072e16484
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2019-11-16 19:32:42 +0100

    add color palatte

---
+++ lib/shared/ColorPalatte.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorPalatte {
+  static final saphireBlue = Color(0xff1D1E33);
+  static final blueLilac = Color(0xff6C4675);
+  static final emeraldGreen = Color(0xff287233);
+  static final resedaGreen = Color(0xff587233);
+  static final chromeGreen = Color(0xff2E3A23);
+}
